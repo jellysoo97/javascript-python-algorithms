17673444a819f1a9987b7eaa2ad739e0f41b9905
jellysoo97 <jellysoo97@gmail.com>
2023-12-27 20:39:18 +0900

    [CEC] sort algorithms

--- codevolution-course/21-bubble-sort.js
+++ codevolution-course/21-bubble-sort.js
@@ -1,20 +1,20 @@
 // 버블 정렬 Bubble Sort : O(n^2) -> for loop & do-while loop
 // isn't great for sorting
 function bubbleSort(arr) {
-  let isSwiped;
+  let isSwapped;
 
   do {
-    isSwiped = false;
+    isSwapped = false;
     // 마지막 인덱스를 보면 이전 인덱스와 비교
     for (let i = 0; i < arr.length - 1; i++) {
       if (arr[i] > arr[i + 1]) {
         let temp = arr[i];
         arr[i] = arr[i + 1];
         arr[i + 1] = temp;
-        isSwiped = true;
+        isSwapped = true;
       }
     }
-  } while (isSwiped);
+  } while (isSwapped);
 }
 
 const arr = [8, 20, -2, 4, -6];
