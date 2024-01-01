a6ae498606bdff08a861647faecb43616a8c765c
jellysoo97 <jellysoo97@gmail.com>
2024-01-01 23:15:56 +0900

    [PGS] Lv.2 / Practice / 숫자의 표현

--- programmers/level-2/practice/숫자의 표현.js
+++ programmers/level-2/practice/숫자의 표현.js
@@ -1,3 +1,4 @@
+// sol1
 // 효율성 테스트 : 테스트 1 〉	통과 (3.41ms, 36.8MB)
 function solution(n) {
   let pointer = 1;
@@ -24,4 +25,19 @@ function solution(n) {
   }
 
   return cnt + 1;
+}
+
+// sol2 : 테스트 1 〉	통과 (0.25ms, 33.3MB)
+// 주어진 수의 홀수인 약수의 개수와 동일 -> 약수를 중간값으로 가지는 연속된 자연수 찾기
+// 21 반례 있긴 함
+function solution(n) {
+  let cnt = 0;
+
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0 && i % 2 === 1) {
+      cnt++;
+    }
+  }
+
+  return cnt;
 }
