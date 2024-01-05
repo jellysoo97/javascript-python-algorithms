4726e06fbc07b4c584922c8eaf81bc9cac840e82
jellysoo97 <jellysoo97@gmail.com>
2024-01-05 15:29:38 +0900

    [PGS] Lv.2 / Stack&Queue / 프로세스

--- programmers/level-2/stack&queue/프로세스.js
+++ programmers/level-2/stack&queue/프로세스.js
@@ -1,10 +1,23 @@
+// 몇번째인지가 중요
 function solution(priorities, location) {
-  const arr = priorities;
+  const arr = priorities.map((priority, index) => ({ priority, index }));
   let index = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (index === location) {
-      return index;
+  while (arr.length) {
+    const front = arr.shift();
+
+    // Array.some() : 배열 안의 어떤 요소라도 주어진 판별 함수를 적어도 하나라도 통과하는지 테스트
+    // return boolean
+    if (arr.some((item) => item.priority > front.priority)) {
+      arr.push(front);
+    } else {
+      index++;
+
+      if (front.index === location) {
+        break;
+      }
     }
   }
+
+  return index;
 }
