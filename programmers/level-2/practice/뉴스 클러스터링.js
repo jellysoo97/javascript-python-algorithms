cf739a6a957aa20089e7d70f6d1aa47bb32bf282
jellysoo97 <jellysoo97@gmail.com>
2024-02-21 00:06:53 +0900

    [PGS] Lv.2 / Practice / 뉴스 클러스팅

--- programmers/level-2/practice/뉴스 클러스터링.js
+++ programmers/level-2/practice/뉴스 클러스터링.js
@@ -15,24 +15,21 @@ function solution(str1, str2) {
     Object.entries(count2)
       .map(([str, count]) => (count1[str] ? 0 : count))
       .reduce((prev, acc) => prev + acc, 0);
+  const jaccard = intersection / union || 1;
 
-  console.log("1", count1);
-  console.log("2", count2);
-  console.log("inter", intersection);
-  console.log("union", union);
-
-  return Math.floor((intersection / union) * 65536);
+  return Math.floor(jaccard * 65536);
 }
 
 function generateMultipleSet(str) {
-  const convertedStr = str.replace(/[^a-zA-Z]/g, "").toUpperCase();
   let multipleSet = [];
 
-  for (let i = 0; i < convertedStr.length - 1; i++) {
-    multipleSet.push(`${convertedStr[i]}${convertedStr[i + 1]}`);
+  for (let i = 0; i < str.length - 1; i++) {
+    multipleSet.push(`${str[i]}${str[i + 1]}`);
   }
 
-  return multipleSet;
+  return multipleSet
+    .filter((str) => /^[a-zA-Z]*$/g.test(str))
+    .map((str) => str.toUpperCase());
 }
 
 function countMultipleSet(arr) {
