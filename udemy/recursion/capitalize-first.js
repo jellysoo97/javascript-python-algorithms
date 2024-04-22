06e6c5141b37d970799e07e33781dffc3dac60d2
jellysoo97 <jellysoo97@gmail.com>
2024-04-23 01:38:50 +0900

    [UDM] Section9 / recursion / capitalize-first

--- udemy/recursion/capitalize-first.js
+++ udemy/recursion/capitalize-first.js
@@ -2,12 +2,14 @@ function capitalizeFirst(arr) {
   let result = [];
 
   function helper(arr) {
+    // base case
     if (!arr.length) return;
 
     if (arr.length > 0) {
       result.push(arr[0][0].toUpperCase() + arr[0].substr(1));
     }
 
+    // different input
     helper(arr.slice(1));
   }
 
