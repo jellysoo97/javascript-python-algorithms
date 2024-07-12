269e0e09ff46b530342b270e4ccde68fa22c53ec
jellysoo97 <jellysoo97@gmail.com>
2024-07-12 22:42:16 +0900

    [PGS] Lv.2 / Practice / 최댓값과 최솟값

--- programmers/level-2/practice/최댓값과 최솟값.js
+++ programmers/level-2/practice/최댓값과 최솟값.js
@@ -1,7 +1,9 @@
+// 최댓값과 최솟값
+// 출력: 최댓값과 최솟값
+// 문자열을 숫자 배열로 변환
+// 배열 중 최솟값, 최댓값 리턴
 function solution(s) {
-  const arr = s.split(" ");
-  const min = Math.min(...arr);
-  const max = Math.max(...arr);
+  const arr = s.split(" ").map(Number);
 
-  return `${min} ${max}`;
+  return `${Math.min(...arr)} ${Math.max(...arr)}`;
 }
