cbb3458408c0e6cd88ab93086bc7ac631d19be3e
jellysoo97 <jellysoo97@gmail.com>
2024-07-12 22:43:16 +0900

    [PGS] Lv.2 / Practice / 이진 변환 반복하기

--- programmers/level-2/practice/이진 변환 반복하기.js
+++ programmers/level-2/practice/이진 변환 반복하기.js
@@ -1,50 +1,25 @@
-// sol1
+// 이진 변환 반복하기
+// 출력: 이진 변환 횟수, 제거된 0의 개수
+// 이진 변환 함수 -> 횟수 카운트, 제거한 0의 개수 카운트
+// s가 "1"이 될 때까지 함수 실행 반복
 function solution(s) {
-  let cnt = 0;
-  let zeroCnt = 0;
+  let count = 0;
+  let zeroCount = 0;
 
-  while (s !== "1") {
-    cnt++;
-
-    for (let i = 0; i < s.length; i++) {
-      if (s[i] === "0") {
-        zeroCnt++;
-      }
-    }
-
-    let removeZeroS = s
-      .split("")
-      .filter((str) => str !== "0")
-      .join("");
-    let length = removeZeroS.length;
-    let binary = "";
-
-    while (length > 0) {
-      binary = (length % 2) + binary;
-      length = Math.floor(length / 2);
-    }
-
-    s = binary;
+  function convertToBinary(str) {
+    count++;
+    // str.match(/0/g)?.length -> match() 결과가 null이면 undefined
+    zeroCount += str.match(/0/g)?.length || 0;
+    return str.replaceAll("0", "").length.toString(2);
   }
 
-  return [cnt, zeroCnt];
-}
-
-// sol2
-function solution(s) {
-  let result = [0, 0];
-
   while (s !== "1") {
-    result[0]++;
-    // match() : 정규식 조건에 맞는 문자열 배열을 반환
-    // 없으면 null 반환 -> || []으로 null.length 에러 핸들링
-    result[1] += (s.match(/0/g) || []).length;
-
-    // '0'을 없애고 이진법으로 변환
-    // Object.prototype.toString() : 문자열 반환
-    // 매개변수로 2~36까지의 기수를 받는다 -> 10진수를 n진수로 변환해서 반환한다
-    s = s.replace(/0/g, "").length.toString(2);
+    s = convertToBinary(s);
   }
 
-  return result;
+  return [count, zeroCount];
 }
+
+solution("110010101001");
+solution("01110");
+solution("1111111");
