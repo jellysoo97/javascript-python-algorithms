cab3ed99d6ac4b26b544e05aa92e7102911de9f1
jellysoo97 <jellysoo97@gmail.com>
2024-07-12 22:44:22 +0900

    [PGS] Lv.2 / Practice / 최솟값 만들기

--- programmers/level-2/practice/최솟값 만들기.js
+++ programmers/level-2/practice/최솟값 만들기.js
@@ -1,20 +1,14 @@
-// sol1
-function solution(A, B) {
-  const sortedA = A.sort((a, b) => a - b);
-  const sortedB = B.sort((a, b) => b - a);
-  let sum = 0;
-
-  for (let i = 0; i < sortedA.length; i++) {
-    sum += sortedA[i] * sortedB[i];
-  }
-
-  return sum;
-}
-
-// sol2
+// 최솟값 만들기
+// 출력: 선택한 두 요소를 곱의 누적합 최솟값
+// 곱의 누적합 최소 = 곱의 최소 = 최소 * 최대
+// A 오름차순 정렬, B 내림차순 정렬
+// reduce -> A[i] * B[i] 누적합
 function solution(A, B) {
   A.sort((a, b) => a - b);
   B.sort((a, b) => b - a);
 
-  return A.reduce((acc, curr, index) => acc + curr * B[index], 0);
+  return A.reduce((prev, cur, i) => prev + cur * B[i], 0);
 }
+
+console.log(solution([1, 4, 2], [5, 4, 4]));
+console.log(solution([1, 2], [3, 4]));
