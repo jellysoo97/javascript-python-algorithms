55f83d6b08c3aa464243a89836a78309d28d9d28
jellysoo97 <jellysoo97@gmail.com>
2024-07-16 23:43:52 +0900

    [PGS] Lv.2 / Practice / 숫자의 표현

--- programmers/level-2/practice/숫자의 표현.js
+++ programmers/level-2/practice/숫자의 표현.js
@@ -1,43 +1,43 @@
-// sol1
-// 효율성 테스트 : 테스트 1 〉	통과 (3.41ms, 36.8MB)
+// 숫자의 표현
+// 출력: 자연수 n을 연속된 자연수들의 합으로 표현하는 방법의 개수
+// 1~n/2까지 반복
+// i~i+k까지의 합 < n이면 k++ 반복, 합 = n이면 카운트
+// 마지막에 n 하나 카운트
+// sol1: 시간초과(87.5%)
 function solution(n) {
-  let pointer = 1;
-  let sum = 0;
-  let cnt = 0;
+  let count = 0;
 
-  while (pointer <= Math.floor(n / 2)) {
-    sum = 0;
+  for (let i = 1; i <= Math.floor(n / 2); i++) {
+    let sum = i;
+    let k = i;
 
-    for (let i = pointer; i <= Math.ceil(n / 2); i++) {
-      sum += i;
-
-      if (sum > n) {
-        break;
-      }
-
-      if (sum === n) {
-        cnt++;
-        break;
-      }
-    }
-
-    pointer++;
-  }
-
-  return cnt + 1;
-}
-
-// sol2 : 테스트 1 〉	통과 (0.25ms, 33.3MB)
-// 주어진 수의 홀수인 약수의 개수와 동일 -> 약수를 중간값으로 가지는 연속된 자연수 찾기
-// 21 반례 있긴 함
-function solution(n) {
-  let cnt = 0;
-
-  for (let i = 1; i <= n; i++) {
-    if (n % i === 0 && i % 2 === 1) {
-      cnt++;
+    while (sum < n) {
+      k++;
+      sum += k;
+      if (sum === n) count++;
     }
   }
 
-  return cnt;
+  return count + 1;
 }
+
+// sol2
+function solution(n) {
+  let count = 0;
+
+  for (let i = 1; i < n / 2; i++) {
+    let sum = i;
+    let j = i;
+
+    while (true) {
+      j++;
+      sum += j;
+      if (sum === n) count++;
+      if (sum + j + 1 > n) break;
+    }
+  }
+
+  return count + 1;
+}
+
+solution(15);
