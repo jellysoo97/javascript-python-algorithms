515dd1fa79309a6a9173213fb68bb80e1d3a91f7
jellysoo97 <jellysoo97@gmail.com>
2024-07-17 17:09:31 +0900

    [PGS] Lv.2 / Greedy / 실습용 로봇

--- programmers/level-2/practice/실습용 로봇.js
+++ programmers/level-2/practice/실습용 로봇.js
@@ -1,27 +1,37 @@
-// 출력: 명령을 모두 수행한 후 마지막 위치
-// R: 오른쪽으로 90도 회전
-// L: 왼쪽으로 90도 회전
-// G: 한 칸 전진
-// B: 한 칸 후진
-// command를 순회
-// R or L: direction 업데이트
-// G or B: coordinate 업데이트
+// 실습용 로봇
+// 출력: 명령을 모두 수행한 뒤 도착한 최종 위치의 좌푯값 [x,y]
+// commands 순회
+// "R" or "L" -> direction 변경, 왼쪽으로 90 회전 = 오른쪽으로 90씩 3번 회전
+// "G" or "B" -> location 이동, direction과 command에 따라 이동
 function solution(commands) {
+  let location = [0, 0];
   let direction = 0;
-  let coordinate = [0, 0];
+  const moves = [
+    { G: [0, 1], B: [0, -1] },
+    { G: [1, 0], B: [-1, 0] },
+    { G: [0, -1], B: [0, 1] },
+    { G: [-1, 0], B: [1, 0] },
+  ];
 
   for (const command of commands.split("")) {
-    if (command === "R") direction += 1;
-    else if (command === "L") direction -= 1;
-    else {
-      direction %= 4;
-      if (direction < 0) direction += 4;
-      if (direction === 0) coordinate[1] += command === "G" ? 1 : -1;
-      else if (direction === 1) coordinate[0] += command === "G" ? 1 : -1;
-      else if (direction === 2) coordinate[1] += command === "G" ? -1 : 1;
-      else coordinate[0] += command === "G" ? -1 : 1;
+    switch (command) {
+      case "R":
+        direction++;
+        direction %= 4;
+        break;
+      case "L":
+        direction += 3;
+        direction %= 4;
+        break;
+      case "G":
+      case "B":
+        location = location.map((loc, i) => loc + moves[direction][command][i]);
+        break;
     }
   }
 
-  return coordinate;
+  return location;
 }
+
+console.log(solution("GRGLGRG"));
+console.log(solution("GRGRGRB"));
