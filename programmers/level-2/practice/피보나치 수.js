afa501a7bc74e417f72001a3bf7cc8c1f58f62dd
jellysoo97 <jellysoo97@gmail.com>
2024-07-17 12:45:10 +0900

    [PGS] Lv.2 / Practice / 피보나치 수

--- programmers/level-2/practice/피보나치 수.js
+++ programmers/level-2/practice/피보나치 수.js
@@ -1,12 +1,31 @@
-// 문제 설명이 좀..
-// 2 이상의 n이 입력되었을 때, n번째 피보나치 수를 1234567으로 나눈 나머지를 리턴하는 함수, solution을 완성해 주세요.
-// 마지막에 n번째 피보나치 수 % 1234567이 아니라 (n번째 피보나치 수 % 1234567)가 피보나치 수열 값으로 계속 들어간다
+// 피보나치 수
+// 출력: n번째 피보나치 수를 1234567로 나눈 나머지
+// F(n) = F(n-1) + F(n-2), F(0) = 0, F(1) = 1
+// sol1 : 재귀 -> 시간초과
+// base case: n <= 2, n === 2 ? return n - 1 : return n
+// different input: return F(n-1) + F(n-2)
+// function solution(n) {
+//   function recursion(k) {
+//     if (k <= 2) return k === 2 ? k - 1 : k;
+
+//     return recursion(k - 1) + recursion(k - 2);
+//   }
+
+//   return recursion(n) % 1234567;
+// }
+
+// sol2
+// 2 <= n <= 100,000 -> BigInt 고려해야 함, 15자리(10^15)를 넘어가면 Number 타입으로 정확한 정수를 보장하기 어려움
+// 나머지를 넣기
 function solution(n) {
-  let arr = [0, 1];
+  let fibonacci = [0, 1];
 
   for (let i = 2; i <= n; i++) {
-    arr[i] = (arr[i - 2] + arr[i - 1]) % 1234567;
+    fibonacci[i] = (fibonacci[i - 1] + fibonacci[i - 2]) % 1234567;
   }
 
-  return arr[n];
+  return fibonacci[n];
 }
+
+console.log(solution(3));
+console.log(solution(5));
