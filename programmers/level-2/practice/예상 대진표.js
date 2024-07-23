6a73d06112c979a840d5c2471aaed64b94c72b41
jellysoo97 <jellysoo97@gmail.com>
2024-07-23 15:11:18 +0900

    [PGS] Lv.2 / Practice / 예상 대진표

--- programmers/level-2/practice/예상 대진표.js
+++ programmers/level-2/practice/예상 대진표.js
@@ -1,17 +1,22 @@
+// 예상 대진표
+// 라운드 -> 카운트+1, 짝수면 a / 2 , 홀수면 (a+1) / 2
+// a, b가 붙는다 -> 같은 조이고 a, b의 차이가 1
 function solution(n, a, b) {
-  let cnt = 0;
-  let bigger = Math.max(a, b);
-  let smaller = Math.min(a, b);
+  let result = 0;
 
-  for (let i = 1; i < Math.sqrt(n) + 1; i++) {
-    if (smaller % 2 === 1 && bigger - 1 === smaller) {
-      cnt = i;
-      break;
-    }
+  while (result < Math.sqrt(n)) {
+    if (Math.ceil(a / 2) === Math.ceil(b / 2) && Math.abs(a - b) === 1) break;
 
-    bigger = Math.ceil(bigger / 2);
-    smaller = Math.ceil(smaller / 2);
+    result++;
+    a = getNextRoundNumber(a);
+    b = getNextRoundNumber(b);
   }
 
-  return cnt;
+  return result + 1;
 }
+
+function getNextRoundNumber(k) {
+  return k % 2 === 0 ? k / 2 : (k + 1) / 2;
+}
+
+solution(8, 4, 7);
