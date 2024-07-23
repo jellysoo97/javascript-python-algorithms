c2d29eda8c30d10af5d16292966581888d79a176
jellysoo97 <jellysoo97@gmail.com>
2024-07-23 15:26:44 +0900

    [PGS] Lv.2 / Practice / 귤 고르기

--- programmers/level-2/practice/귤 고르기.js
+++ programmers/level-2/practice/귤 고르기.js
@@ -1,59 +1,27 @@
-// sol1
+// 귤 고르기
+// 서로 다른 크기의 귤 가짓수 최솟값
+// 크기-개수 딕셔너리
+// 딕셔너리 배열화 -> 내림차순 정렬 -> k - 개수, result 카운트 반복 -> k <= 0이면 종료
 function solution(k, tangerine) {
-  const sortedArr = tangerine.sort((a, b) => a - b);
-  let cnt = [1];
-
-  for (let i = 0; i < sortedArr.length - 1; i++) {
-    if (sortedArr[i] === sortedArr[i + 1]) {
-      cnt[cnt.length - 1]++;
-    } else {
-      cnt.push(1);
-    }
-  }
-
-  cnt = cnt.sort((a, b) => b - a);
+  let dict = {};
   let result = 0;
-  let index = 0;
 
-  while (k > 0) {
-    if (k - cnt[index] <= 0) {
-      result++;
-      break;
-    }
+  // tangerine.forEach((elem) => {
+  //   if (!dict[elem]) dict[elem] = 1;
+  //   else dict[elem]++;
+  // });
+  tangerine.forEach((elem) => (dict[elem] = (dict[elem] || 0) + 1));
 
-    k -= cnt[index];
+  for (const count of Object.values(dict).sort((a, b) => b - a)) {
     result++;
-    index++;
+    k -= count;
+
+    if (k <= 0) break;
   }
 
   return result;
 }
 
-// sol2 -> faster
-// 테스트 1 〉	통과 (3.01ms, 37.2MB)
-// 테스트 2 〉	통과 (3.61ms, 37.2MB)
-// 테스트 3 〉	통과 (2.74ms, 37.3MB)
-// 테스트 4 〉	통과 (3.49ms, 37.3MB)
-// 배열내 중복 요소 개수 구하기
-// object key-value로 접근 -> key가 같은 value + 1
-// dict = {}
-// arr.forEach((elem) => dict[elem] = (dict[elem] || 0) + 1)
-function solution(k, tangerine) {
-  let result = 0;
-  const dict = {};
-
-  tangerine.forEach((t) => (dict[t] = (dict[t] || 0) + 1));
-  const cntArr = Object.values(dict).sort((a, b) => b - a);
-
-  for (const cnt of cntArr) {
-    result++;
-
-    if (cnt < k) {
-      k -= cnt;
-    } else {
-      break;
-    }
-  }
-
-  return result;
-}
+solution(6, [1, 3, 2, 5, 4, 5, 2, 3]);
+solution(4, [1, 3, 2, 5, 4, 5, 2, 3]);
+solution(2, [1, 1, 1, 1, 2, 2, 2, 3]);
