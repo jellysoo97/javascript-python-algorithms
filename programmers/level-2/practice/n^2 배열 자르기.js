82ce49ad55998035371cb83aaa6188fac42022a2
jellysoo97 <jellysoo97@gmail.com>
2024-07-24 15:07:01 +0900

    [PGS] Lv.2 / Practice / n^2 배열 자르기

--- programmers/level-2/practice/n^2 배열 자르기.js
+++ programmers/level-2/practice/n^2 배열 자르기.js
@@ -1,25 +1,19 @@
-// sol1
-function solution(n, left, right) {
-  let arr = [];
-
-  for (let i = 0; i < right - left + 1; i++) {
-    let row = Math.floor((left + i) / n);
-    let col = (left + i) % n;
-    arr.push(col < row ? row + 1 : col + 1);
-  }
-
-  return arr;
-}
-
-// sol2
-// i % n 인덱스까지는 Math.floor(i / n) + 1 즉, 몇번째 줄인지가 들어갈 것이고
-// 그 이후부터는 i % n 값이 들어감
+// n^2 배열 자르기
+// k행, k열 -> 값이 k
+// 1차원 배열 인덱스 i: 행 > 열이면 행, 아니면 열
 function solution(n, left, right) {
   let arr = [];
 
   for (let i = left; i <= right; i++) {
-    arr.push(Math.max(i % n, Math.floor(i / n)) + 1);
+    const row = Math.floor(i / n) + 1;
+    const col = (i % n) + 1;
+
+    // arr.push(Math.max(row, col))
+    arr.push(row > col ? row : col);
   }
 
   return arr;
 }
+
+solution(3, 2, 5);
+solution(4, 7, 14);
