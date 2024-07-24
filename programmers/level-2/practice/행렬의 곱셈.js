e2412b980e64f7b14a6f4dce27cecd2f636ad63c
jellysoo97 <jellysoo97@gmail.com>
2024-07-24 15:07:11 +0900

    [PGS] Lv.2 / Practice / 행렬의 곱셈

--- programmers/level-2/practice/행렬의 곱셈.js
+++ programmers/level-2/practice/행렬의 곱셈.js
@@ -1,22 +1,34 @@
+// 행렬의 곱셈
+// i행 j열 * j행 * i열의 합
 function solution(arr1, arr2) {
-  const row = arr1.length;
-  const col = arr2[0].length;
-  let arr = [];
+  const rowLength = arr1.length;
+  const colLength = arr2[0].length;
   let result = [];
 
-  // 1. row번 반복
-  for (let i = 0; i < row; i++) {
-    arr = [];
-    // 2. col번 반복
-    for (let j = 0; j < col; j++) {
-      // 3. sum 구하기
-      arr.push(
-        arr1[i].reduce((acc, cur, index) => acc + cur * arr2[index][j], 0)
+  for (let i = 0; i < rowLength; i++) {
+    let row = [];
+
+    for (let j = 0; j < colLength; j++) {
+      row.push(
+        arr1[i].reduce((prev, cur, index) => prev + cur * arr2[index][j], 0)
       );
     }
 
-    result.push(arr);
+    result.push(row);
   }
 
   return result;
 }
+
+solution(
+  [
+    [2, 3, 2],
+    [4, 2, 4],
+    [3, 1, 4],
+  ],
+  [
+    [5, 4, 3],
+    [2, 4, 1],
+    [3, 1, 1],
+  ]
+);
