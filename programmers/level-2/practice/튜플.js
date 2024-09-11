7f519df84abf333a34e27676cbc7806285cea475
jellysoo97 <jellysoo97@gmail.com>
2024-09-11 15:52:56 +0900

    [PGS] Lv.2 / Practice / 튜플

--- programmers/level-2/practice/튜플.js
+++ programmers/level-2/practice/튜플.js
@@ -1,19 +1,22 @@
+// "{, }" 제거 -> ,로 split -> 2차원 배열로 변경
+// 길이 순으로 오름차순 정렬
+// a1, a2, a3, ...an 조합
 function solution(s) {
-  const arr = s
-    .slice(2, -2)
-    .split("},{")
-    .sort((a, b) => a.length - b.length);
   let result = [];
 
-  for (const elem of arr) {
-    const temp = elem.split(",");
+  s = s
+    .split("},")
+    .map((elem) =>
+      elem.replaceAll("{", "").replaceAll("}", "").split(",").map(Number)
+    );
+  s.sort((a, b) => a.length - b.length);
 
-    for (const str of temp) {
-      if (!result.includes(str)) {
-        result.push(str);
-      }
-    }
-  }
+  s.forEach((elem) => {
+    const newStr = elem.find((str) => !result.includes(str));
+    if (newStr) result.push(newStr);
+  });
 
-  return result.map((str) => +str);
+  return result;
 }
+
+solution("{{2},{2,1},{2,1,3},{2,1,3,4}}");
