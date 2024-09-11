8267dff7c66ee556145365ed1a337e3e1c8ee074
jellysoo97 <jellysoo97@gmail.com>
2024-09-11 14:01:48 +0900

    [PGS] Lv.2 / Sort / H-Index

--- programmers/level-2/sort/H-Index.js
+++ programmers/level-2/sort/H-Index.js
@@ -1,10 +1,14 @@
+// h번 이상 인용된 논문이 h편 이상, 나머지는 h편 이하 인용
+// h번 이상 인용된 논문 수 <= 인용횟수이면 loop -> HIndex++
 function solution(citations) {
-  const sortedArr = citations.sort((a, b) => b - a);
   let hIndex = 0;
+  citations.sort((a, b) => b - a);
 
-  while (hIndex + 1 <= sortedArr[hIndex]) {
+  while (hIndex + 1 <= citations[hIndex]) {
     hIndex++;
   }
 
   return hIndex;
 }
+
+solution([3, 0, 6, 1, 5]);
