1b75ec22038875d6c7933c08e83213f0d3fc8176
jellysoo97 <jellysoo97@gmail.com>
2024-09-12 15:08:23 +0900

    [PGS] Lv.2 / Stack&Queue / 프로세스

--- programmers/level-2/stack&queue/프로세스.js
+++ programmers/level-2/stack&queue/프로세스.js
@@ -1,3 +1,23 @@
+// sol1
+// 우선순위 shift -> 우선순위 비교
+// 가장 높은 우선순위면 실행 스택에 push
+// 아니면 우선순위 tail에 push
+function solution(priorities, location) {
+  let arr = priorities.map((priority, index) => [priority, index]);
+  let stack = [];
+
+  while (stack.length < priorities.length) {
+    const cur = arr.shift();
+
+    if (Math.max(...arr.map(([priority]) => priority)) <= cur[0])
+      stack.push(cur);
+    else arr.push(cur);
+  }
+
+  return stack.indexOf(stack.find(([_, index]) => index === location)) + 1;
+}
+
+// sol2
 // 몇번째인지가 중요
 function solution(priorities, location) {
   const arr = priorities.map((priority, index) => ({ priority, index }));
@@ -21,3 +41,6 @@ function solution(priorities, location) {
 
   return index;
 }
+
+solution([2, 1, 3, 2]);
+solution([1, 1, 9, 1, 1, 1]);
